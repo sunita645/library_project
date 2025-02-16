0aa318e339dbbeb9d9c04c523b00d02896fbe629
sunita <sunitabagale2017@gmail.com>
2025-02-16 16:39:00 +0545

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
+
+route::get('/',[HomeController::class,'index']);
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+route::get('/home',[AdminController::class,'index']);
